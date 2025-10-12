8d2d43f53ca83b8321c66ffcfeac5b16c930bd26
kimalstjs1 <kimalstjs1@gmail.com>
2025-10-12 18:02:19 +0900

    5week

---
+++ thisisjava/src/ch05/sec05/EmptyStringExample.java
@@ -0,0 +1,14 @@
+package ch05.sec05;
+
+public class EmptyStringExample {
+
+	public static void main(String[] args) {
+		String hobby = "";
+		if(hobby.equals("")) {
+			System.out.println("hobby 변수가 참조하는 string 객체는 빈문자열");
+			
+		}
+
+	}
+
+}
